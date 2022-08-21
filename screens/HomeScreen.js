b8f947718a7f77bee974a80a67b4b37b9a1591ff
gurtej1992 <gurtej1992@gmail.com>
2022-08-21 11:48:11 -0400

    Added Flat list for grid image gallery.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet,FlatList,Image } from "react-native";
 import { TextInput } from 'react-native-paper';
 import Button from "../componets/Button";
 import { GlobalStyles } from "../constants/style";
+import React, { useEffect, useState } from "react";
 
 function HomeScreen() {
+    const [dataSource, setDataSource] = useState([]);
+ 
+    useEffect(() => {
+      let items = Array.apply(null, Array(9)).map((v, i) => {
+        return {
+          id: i,
+          src: 'http://via.placeholder.com/200x200?text=' + (i + 1)
+        };
+      });
+      setDataSource(items);
+    }, []);
     return (<View style={styles.container}>
         <Text style={styles.heading}>Picture of the day:</Text>
         <Text style={styles.subheading}>Search for Astronomy: Picture of the day by date.</Text>
@@ -20,7 +32,29 @@ function HomeScreen() {
       />
       <Button>Search</Button>
       <Text style={styles.result}>Results(0):</Text>
-      <Text style={styles.resunoResultlt}>No results found. Enter a start and end date.</Text>
+      
+      {dataSource.length > 0 ? 
+      <FlatList
+      data={dataSource}
+      renderItem={({item}) => (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            margin: 1
+          }}>
+          <Image
+            style={styles.imageThumbnail}
+            source={{uri: item.src}}
+          />
+        </View>
+      )}
+      //Setting the number of column
+      numColumns={3}
+      keyExtractor={(item, index) => index}
+    />
+    : <Text style={styles.resunoResultlt}>No results found. Enter a start and end date.</Text>}
+      
     </View>);
 }
 const styles = StyleSheet.create({
@@ -65,5 +99,13 @@ const styles = StyleSheet.create({
         backgroundColor: GlobalStyles.colors.moonDust50,
         marginBottom: 10,
     },
+    imageThumbnail: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+        width: 100,
+        margin : 5,
+        borderRadius : 5,
+      },
   });
 export default HomeScreen;
