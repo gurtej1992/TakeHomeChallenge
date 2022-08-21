0f11935349855fc7a941be1929621622badcdffa
Tej <gurtej1992@gmail.com>
2022-08-21 08:36:13 -0400

    Added Material UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { TextInput } from 'react-native-paper';
 
 function HomeScreen() {
-    return (<View><Text>HOme</Text></View>);
+    return (<View>
+        <Text>Picture of the day:</Text>
+        <Text>Search for Astronomy: Picture of the day by date.</Text>
+        <TextInput
+        label="Start Date"
+      
+      />
+        <TextInput
+        label="End Date"
+        style={styles.input}
+      />
+    </View>);
 }
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 export default HomeScreen;
